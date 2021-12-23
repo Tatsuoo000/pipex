d4ec8d60b35843a7b66513b1713c213aab2ead62
tatsuoo000 <rush.life29@gmail.com>
2021-12-24 00:01:56 +0900

    init

---
+++ dst/includes/pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkano <tkano@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/23 23:57:36 by tkano             #+#    #+#             */
+/*   Updated: 2021/12/23 23:58:56 by tkano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define ERROR -1
+
+#endif
