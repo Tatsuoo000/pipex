77b5246ae6e5f0a71265a1b8b9777685920978a7
tatsuoo000 <tatsuya.kano29@gmail.com>
2021-12-24 17:03:23 +0900

    error_check

--- dst/includes/pipex.h
+++ dst/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: tkano <tkano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 23:57:36 by tkano             #+#    #+#             */
-/*   Updated: 2021/12/23 23:58:56 by tkano            ###   ########.fr       */
+/*   Updated: 2021/12/24 16:54:46 by tkano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,30 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <sys/wait.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
 
 # define TRUE 1
 # define FALSE 0
 # define SUCCESS 0
 # define ERROR -1
+# define READ 0
+# define WRITE 1
+
+typedef struct s_cmd {
+	char	*cmd;
+	int		pipefd[2];
+	int		pid;
+}			t_cmd;
+
+typedef struct s_info {
+	char	*file1;
+	char	*file2;
+	t_cmd	**cmd;
+	char	**env;
+}			t_info;
+
 
 #endif
